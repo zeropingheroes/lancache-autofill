97a49c3ddaf1c3b553ec09e5c14d79d92a87f22b
ilumos <ilumos@gmail.com>
2017-08-12 21:16:46 +0100

    Created default download list from top 100 free apps

--- steam/get_top_100_apps.sh
+++ steam/get_top_100_apps.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 URL="http://steamspy.com/api.php?request=top100in2weeks"
 
-echo "Getting list of free apps..."
-curl -s $URL | jq '[.[] | select(.price == "0") | {id: .appid, name}]' > $DIR/apps_free.json
+echo "Downloading list of top 100 free apps..."
+curl -s $URL | jq '[.[] | select(.price == "0") | {id: .appid, name}]' > $SCRIPT_DIR/top_100_free_apps.json
 
-echo "Getting list of paid apps..."
-curl -s $URL | jq '[.[] | select(.price != "0") | {id: .appid, name}]' > $DIR/apps_paid.json
+echo "Downloading list of top 100 paid apps..."
+curl -s $URL | jq '[.[] | select(.price != "0") | {id: .appid, name}]' > $SCRIPT_DIR/top_100_paid_apps.json
 
+echo "Creating default app download list from list of top 100 free apps..."
+cp $SCRIPT_DIR/top_100_free_apps.json $SCRIPT_DIR/app_download_list.json
