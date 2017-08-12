6e3e15ac8c0252eb6643328d89361e9f6a903494
ilumos <ilumos@gmail.com>
2017-08-12 21:17:57 +0100

    Updating SteamCMD URL and updating installed dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-apt install -y lib32gcc1 jq
+apt install -y lib32gcc1 lib32stdc++6 lib32tinfo5 lib32ncurses5 libcurl3-gnutls:i386 jq
 
 mkdir -p /usr/games/steam
-cd /usr/games/steam
-curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+
+URL="http://media.steampowered.com/client/steamcmd_linux.tar.gz"
+
+cd /usr/games/steam && curl -sqL $URL | tar zxvf -
