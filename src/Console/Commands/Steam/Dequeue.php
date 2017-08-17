781f3cb4733e8c1e98717074613365f815bbfe31
ilumos <ilumos@gmail.com>
2017-08-17 07:22:35 +0100

    Increased flexibility of dequeuing

---
+++ src/Console/Commands/Steam/Dequeue.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Zeropingheroes\LancacheAutofill\Console\Commands\Steam;
+
+use Illuminate\Console\Command;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Dequeue extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'steam:dequeue
+                            {--app_id=}
+                            {--platform=}
+                            {--account=}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove item(s) from the download queue';
+
+    /**
+     * The permissible platforms.
+     *
+     * @var array
+     */
+    const PLATFORMS = ['windows', 'osx', 'linux'];
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    { 
+        if( $this->option('platform') && ! in_array($this->option('platform'), $this::PLATFORMS))
+        {
+            $this->error('Invalid platform specified. Available platforms are: '. implode(' ', $this::PLATFORMS));
+            die();
+        }
+
+        $query = Capsule::table('steam_queue');
+
+        if( $this->option('app_id') )
+            $query->where('appid', $this->option('app_id'));
+
+        if( $this->option('platform') )
+            $query->where('platform', $this->option('platform'));
+
+        if( $this->option('account') )
+            $query->where('account', $this->option('account'));
+                
+        $affected = $query->delete();
+
+        if( ! $affected ) {
+            $this->error('No items in the queue match the provided criteria');
+            die();
+        }
+
+        $this->info('Removed ' . $affected .' item(s) from the download queue');
+    }
+}
