4ac3252ee317b1449272cdf16d1c9ed000616339
ilumos <ilumos@gmail.com>
2017-08-17 01:00:45 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GREEN='\033[0;32m'
+BLACK='\033[0m'
 
-apt install -y lib32gcc1 lib32stdc++6 lib32tinfo5 lib32ncurses5 libcurl3-gnutls:i386 jq
+printf "${GREEN}Installing dependencies with apt${BLACK}\n"
+apt install -y  lib32gcc1 \
+                lib32stdc++6 \
+                lib32tinfo5 \
+                lib32ncurses5 \
+                libcurl3-gnutls:i386 \
+                php7.0-cli \
+                php7.0-mbstring \
+                php7.0-sqlite \
+                composer \
+                expect \
 
-mkdir -p /usr/games/steam
+printf "${GREEN}Installing dependencies with Composer${BLACK}\n"
+composer update
 
-URL="http://media.steampowered.com/client/steamcmd_linux.tar.gz"
+printf "${GREEN}Installing Steam${BLACK}\n"
+mkdir -p /usr/games/steam && cd /usr/games/steam && curl -sqL "http://media.steampowered.com/client/steamcmd_linux.tar.gz" | tar zxvf -
 
-cd /usr/games/steam && curl -sqL $URL | tar zxvf -
+
+printf "${GREEN}Creating database file${BLACK}\n"
+cd $SCRIPT_DIR && touch "database.sqlite"
+
+printf "${GREEN}Creating your enviroment file${BLACK}\n"
+cd $SCRIPT_DIR && cp ".env.example" ".env" && /bin/nano ".env"
