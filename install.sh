0027d752a8034e7e2c31ae841fe457877172161a
ilumos <ilumos@gmail.com>
2017-12-09 02:49:42 +0000

    Fixing typo

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd $SCRIPT_DIR && composer install
 printf "${GREEN}Creating database file${BLACK}\n"
 cd $SCRIPT_DIR && touch "database.sqlite"
 
-printf "${GREEN}Creating your enviroment file${BLACK}\n"
+printf "${GREEN}Creating your environment file${BLACK}\n"
 cd $SCRIPT_DIR && cp ".env.example" ".env"
 
 cd $SCRIPT_DIR && ./lancache-autofill app:initialise-database
