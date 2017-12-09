ffbb82696186de8a18da3bf6e9ee79d86f1c88dc
ilumos <ilumos@gmail.com>
2017-12-09 01:01:34 +0000

    Adding dummy exception handler

---
+++ src/Exceptions/Handler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zeropingheroes\LancacheAutofill\Exceptions;
+
+use Exception;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+         // parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        // return parent::render($request, $exception);
+    }
+}
