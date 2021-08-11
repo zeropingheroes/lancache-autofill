705cb4da3af7736d7b8fff023f0c34711f2f55f0
Donnie McMahan <mcmahanjd@pella.com>
2021-08-11 15:34:09 -0500

    Add QueueUsersOwnedApps to Artisan commands

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -5,7 +5,7 @@ namespace Zeropingheroes\LancacheAutofill\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Zeropingheroes\LancacheAutofill\Commands\Steam\{
-    AuthoriseAccount, Dequeue, Initialise, QueueApp, QueuePopularApps, QueueUsersRecentApps, Requeue, SearchApps, ShowQueue, StartDownloading, UpdateAppList
+    AuthoriseAccount, Dequeue, Initialise, QueueApp, QueuePopularApps, QueueUsersRecentApps, QueueUsersOwnedApps, Requeue, SearchApps, ShowQueue, StartDownloading, UpdateAppList
 };
 use Zeropingheroes\LancacheAutofill\Commands\App\{
     InitialiseDatabase, InitialiseDownloadsDirectory
@@ -28,6 +28,7 @@ class Kernel extends ConsoleKernel
         QueueApp::class,
         QueuePopularApps::class,
         QueueUsersRecentApps::class,
+        QueueUsersOwnedApps::class,
         ShowQueue::class,
         StartDownloading::class,
         AuthoriseAccount::class,
